7c1e5a5794302bca84f9526fbacc450c91041725
michaelwang0213 <michaelwang0213@yahoo.com>
2017-10-25 20:07:20 -0700

    updated website

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function handleImageClick(event) {
 		target.addClass('selected');
 		target.removeClass('not-selected');
 
-		info.fadeIn(2000);
+		info.fadeIn(20000);
 		preview.css('background-image', target.css('background-image'));
 	}
 }
